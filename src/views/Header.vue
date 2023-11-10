2916e0c9aed609e95f511f5aaa5cd239c2cf338d
firdavs <frontendhtml1234@gmail.com>
2023-11-10 21:15:05 +0500

    new project

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -7,43 +7,7 @@
 </template>
 
 <script setup>
-import "primevue/resources/themes/lara-light-teal/theme.css";
 
-import Menubar from "primevue/menubar";
-import { ref } from "vue";
-
-const items = ref([
-  {
-    label: "Home",
-    icon: "pi pi-fw pi-pencil",
-  },
-  {
-    label: "About Me",
-    icon: "pi pi-fw pi-user",
-    items: [
-      {
-        label: "Web Developer",
-        icon: "pi pi-fw pi-user-plus",
-      },
-      {
-        label: "Frontend Developer",
-        icon: "pi pi-fw pi-user-minus",
-      },
-      {
-        label: "Strong JUNIOR",
-        icon: "pi pi-fw pi-users",
-      },
-    ],
-  },
-  {
-    label: "Contact",
-    icon: "pi pi-fw pi-calendar",
-  },
-  {
-    label: "Quit",
-    icon: "pi pi-fw pi-power-off",
-  },
-]);
 </script>
 
 <style scoped>
@@ -56,9 +20,8 @@ const items = ref([
 
 @media (min-width: 320px) and (max-width: 576px){
   .card{
-    width: 100%;
-    height: auto;
-  }
+  background-color: #240145;
+}
 }
 @media (min-width: 576px) and (max-width: 768px){
   .card{
@@ -66,13 +29,4 @@ const items = ref([
     height: auto;
   }
 }
-@media (min-width: 768px) and (max-width: 992px){
-  .card{
-    width: 100%;
-    height: auto;
-  }
-}
-
- 
- 
 </style>
