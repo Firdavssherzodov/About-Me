5343b7bf532848a86d1b9f16d05b035f7476d8d2
firdavs <frontendhtml1234@gmail.com>
2023-11-13 13:38:05 +0500

    salimjon

--- src/views/Section_two.vue
+++ src/views/Section_two.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="diva">
 <p class="text-center fs-6 fw-bold p1 pt-5" style="color: #8c8989; font-family: Poppins,sans-serif;"> Menda bor ko'nikmalar</p>
-<h1 class="text-center h11" style="color: white; font-size: 2rem; font-family: Poppins,sans-serif;">Mening Tajribam</h1>
+<h1 class="text-center h11 " style="color: white; font-size: 2rem; font-family: Poppins,sans-serif;">Mening Tajribam</h1>
     </div>
 </template>
 
