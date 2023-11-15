d6e85575652193a9e49efdaed5bbfedfafec81de
firdavs <frontendhtml1234@gmail.com>
2023-11-15 21:53:45 +0500

    hjg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
 <HeaderVue/>
-<abou1t/>
+
 <content1/>
 <content2/>
 <content3/>
@@ -17,7 +17,7 @@
 
 <script setup>
 import HeaderVue from './Header.vue'
-import abou1t from '../views/abouts.vue'
+
 import content1 from '../views/Section_one.vue'
 import content2 from '../views/Section_two.vue'
 import content3 from '../views/Section_three.vue'
