d8fbfb0c0456bce4f5f9d58c49a8833614f60fcf
firdavs <frontendhtml1234@gmail.com>
2023-12-20 19:24:47 +0500

    news

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,13 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
 
+//     {
+// path:'/',
+// name:'login',
+// component:()=> import('../Login.vue')
+//     },
     {
-path:'/',
-name:'login',
-component:()=> import('../Login.vue')
-    },
-    {
-      path: "/home",
+      path: "/",
       name: "home",
       component: () => import("../views/Home.vue"),
     },
